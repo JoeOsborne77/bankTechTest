94b4b9ba77bec8ffabf59435132a5ce425f7b409
JoeOsborne77 <joeosborne77@gmail.com>
2022-11-14 11:20:46 +0000

    Refactored date tests to contain new Date().toLocaleDateString(en-UK) instead of a specific date

--- spec/account.test.js
+++ spec/account.test.js
@@ -1,27 +1,37 @@
 const Account = require("../src/account");
 
-describe("when user displays statement", () => {
-  it("records deposits ", () => {
+describe("when user makes deposit", () => {
+  it("records deposits", () => {
     const account = new Account();
     account.makeDeposit(500);
     expect(account.showDeposits()).toContain(500);
   });
 
-  it("records date of deposits ", () => {
+  it("records date of deposits", () => {
     const account = new Account();
     account.makeDeposit(500);
-    expect(account.showDeposits()).toContain("13/11/2022");
+    expect(account.showDeposits()).toContain(
+      new Date().toLocaleDateString("en-UK")
+    );
   });
 
-  it("records withdrawals ", () => {
+  it("records withdrawal amount", () => {
     const account = new Account();
     account.makeWithdrawal(200);
     expect(account.showWithdrawals()).toContain(200);
   });
 
-  it("records withdrawals ", () => {
+  it("records withdrawal date", () => {
     const account = new Account();
     account.makeWithdrawal(200);
-    expect(account.showWithdrawals()).toContain("13/11/2022");
+    expect(account.showWithdrawals()).toContain(
+      new Date().toLocaleDateString("en-UK")
+    );
+  });
+
+  it("updates balance based on deposit", () => {
+    const account = new Account();
+    account.makeDeposit(200);
+    expect(account.showBalance()).toEqual(200);
   });
 });
