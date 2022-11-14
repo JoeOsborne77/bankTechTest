805f7be54922d5da9514ef2392507f76bbf5b59c
JoeOsborne77 <joeosborne77@gmail.com>
2022-11-14 13:45:17 +0000

    statement class renamed to App

---
+++ spec/app.test.js
@@ -0,0 +1,22 @@
+const App = require("../src/app");
+
+describe("Given new account instance", () => {
+  it("displays deposits", () => {
+    const app = new App();
+    app.deposit(700);
+    expect(app.showDeposits()).toEqual([
+      new Date().toLocaleDateString("en-UK"),
+      700,
+    ]);
+  });
+
+  it("displays withdrawals", () => {
+    const app = new App();
+    app.deposit(700);
+    app.withdraw(350);
+    expect(app.showWithdrawals()).toEqual([
+      new Date().toLocaleDateString("en-UK"),
+      350,
+    ]);
+  });
+});
